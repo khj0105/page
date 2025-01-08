dc8e61109a73b2e184bcfd5c070d28353e1ff58d
user <khj0001052@gmail.com>
2025-01-08 21:11:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='body'>
+      <header>
+        <div className='inbox'>
+        <h1 className='logo'><Link to = "/">LOGO</Link></h1>
+        <nav className='nav'>
+          <ul>
+            <li>스토어</li>
+            <li>추천제품</li>
+            <li>상품</li>
+            <li>기획전</li>
+            <li>알림</li>
+            <li>고객서비스</li>
+          </ul>
+        </nav>
+        <info className='info_menu'>
+          <ul>
+            <li>검색</li>
+            <li>마이페이지</li>
+            <li>장바구니</li>
+          </ul>
+        </info>
+        </div>
       </header>
     </div>
   );
